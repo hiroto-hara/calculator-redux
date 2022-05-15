d33f3f6d43cb8ada503c39b0abfb6dd7b94a0011
hiroto-hara <xre0605@gmail.com>
2022-05-15 15:42:15 +0900

    action,type,reducerを定義

---
+++ src/redux/actions/types.js
@@ -0,0 +1,14 @@
+//「数字」がクリックされたとき
+export const INPUT_NUMBER = "INPUT_NUMBER";
+
+//「+,-,×,÷」がクリックされたとき
+export const PLUS = "PLUS";
+export const MINUS = "MINUS";
+export const MULTIPLY = "MULTIPLY";
+export const DIVIDE = "DIVIDE";
+
+//「=」がクリックされたとき
+export const EQUAL = "EQUAL";
+
+//「AC」がクリックされたとき
+export const CLEAR = "CLEAR";
